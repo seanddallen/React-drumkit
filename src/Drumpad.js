62ffdb7ff2c6ca888b0749b712ea7545e733e465
Sean Tayler <seanjtayler@gmail.com>
2019-02-01 18:20:47 -0700

    basic project setup

---
+++ src/Drumpad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Drumpad extends Component {
+  render() {
+    return (
+      <div className="drum-pad" id={this.props.id}>
+        
+      </div>
+    )
+  }
+}
+
+export default Drumpad; 
